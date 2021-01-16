e90a1eb4da27e2cb252f809c8345b59dd5d5fc5b
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-01-16 01:11:50 +0000

    chore(config): add hook to lint code on push

--- .huskyrc.js
+++ .huskyrc.js
@@ -4,5 +4,6 @@ module.exports = {
   hooks: {
     'commit-msg': tasks(['commitlint -E HUSKY_GIT_PARAMS']),
     'pre-commit': tasks(['npm run format:staged']),
+    'pre-push': tasks(['npm run code:lint', 'npm run code:check']),
   },
 };
