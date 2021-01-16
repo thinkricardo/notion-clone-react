ecdbd3258f1ca30111ea4d8fe9345656dedf77f9
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-01-16 00:14:17 +0000

    chore(config): add hook to check commit messages

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,6 +2,7 @@ const tasks = (arr) => arr.join(' && ');
 
 module.exports = {
   hooks: {
+    'commit-msg': tasks(['commitlint -E HUSKY_GIT_PARAMS']),
     'pre-commit': tasks(['npm run format:staged']),
   },
 };
