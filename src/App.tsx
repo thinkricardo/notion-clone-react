e9bdce5b46f05962f9c10ee0a2d0e026bfc4e144
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-01-25 22:11:51 +0000

    feat(page): add editable page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { EditablePage } from './components/editable-page/EditablePage';
+
 import './App.scss';
 
 const App: React.FC = () => {
-  return <h1>Notion Clone React</h1>;
+  return (
+    <>
+      <h1>Notion Clone React</h1>
+      <EditablePage />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/editable-page/EditablePage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const EditablePage: React.FC = () => {
+  return <h2>Page</h2>;
+};
