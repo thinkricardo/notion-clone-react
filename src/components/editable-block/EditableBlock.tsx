26f824cc61d09e5f3f00238118ac774441cc6cc0
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-01-25 22:18:54 +0000

    feat(block): add editable block component

---
+++ src/components/editable-block/EditableBlock.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const EditableBlock: React.FC = () => {
+  return <div contentEditable={true}>Type &apos;/&apos; for commands</div>;
+};
--- src/components/editable-page/EditablePage.tsx
+++ src/components/editable-page/EditablePage.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import { EditableBlock } from '../editable-block/EditableBlock';
+
 export const EditablePage: React.FC = () => {
-  return <h2>Page</h2>;
+  return (
+    <>
+      <h2>Page</h2>
+      <EditableBlock />
+    </>
+  );
 };
