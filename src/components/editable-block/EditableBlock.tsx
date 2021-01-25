d526c98cef790bad78bc90a4c4a23db9204557e4
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-01-25 22:41:13 +0000

    feat(block): store edited block content

--- src/components/editable-block/EditableBlock.tsx
+++ src/components/editable-block/EditableBlock.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 export const EditableBlock: React.FC = () => {
-  return <div contentEditable={true}>Type &apos;/&apos; for commands</div>;
+  const elementRef = useRef<HTMLDivElement>(null);
+
+  const [value, setValue] = useState('Type / for commands');
+
+  const handleInput = () => {
+    if (!elementRef.current) {
+      return;
+    }
+
+    setValue(elementRef.current.innerText);
+  };
+
+  return (
+    <div ref={elementRef} contentEditable={true} onInput={handleInput}>
+      {value}
+    </div>
+  );
 };
