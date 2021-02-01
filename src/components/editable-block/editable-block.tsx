11757809554b8435da97ebae2334f904dcd502d0
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-02-01 22:42:16 +0000

    feat(page): add new blocks at the bottom

--- src/components/editable-block/editable-block.tsx
+++ src/components/editable-block/editable-block.tsx
@@ -1,12 +1,16 @@
-import React, { useRef, useState } from 'react';
+import React, { KeyboardEvent, useRef, useState } from 'react';
 
 import { Block } from './block';
 
 type EditableBlockProps = {
   block: Block;
+  onAddBlock: () => void;
 };
 
-export const EditableBlock: React.FC<EditableBlockProps> = ({ block }) => {
+export const EditableBlock: React.FC<EditableBlockProps> = ({
+  block,
+  onAddBlock,
+}) => {
   const elementRef = useRef<HTMLDivElement>(null);
 
   const [value, setValue] = useState(block.content);
@@ -19,8 +23,20 @@ export const EditableBlock: React.FC<EditableBlockProps> = ({ block }) => {
     setValue(elementRef.current.innerText);
   };
 
+  const handleKeyDown = (evt: KeyboardEvent) => {
+    if (evt.key === 'Enter') {
+      evt.preventDefault();
+      onAddBlock();
+    }
+  };
+
   return (
-    <div ref={elementRef} contentEditable={true} onInput={handleInput}>
+    <div
+      ref={elementRef}
+      contentEditable={true}
+      onInput={handleInput}
+      onKeyDown={handleKeyDown}
+    >
       {value}
     </div>
   );
--- src/components/editable-page/editable-page.tsx
+++ src/components/editable-page/editable-page.tsx
@@ -3,14 +3,25 @@ import React, { useState } from 'react';
 import { EditableBlock, InitialBlock } from '../editable-block';
 
 export const EditablePage: React.FC = () => {
-  const [blocks] = useState([{ ...InitialBlock }]);
+  const [blocks, setBlocks] = useState([InitialBlock]);
+
+  const handleAddBlock = () => {
+    const newBlocks = blocks.concat([InitialBlock]);
+    setBlocks(newBlocks);
+  };
 
   return (
     <>
       <h2>Page</h2>
 
       {blocks.map((block, index) => {
-        return <EditableBlock key={index} block={block} />;
+        return (
+          <EditableBlock
+            key={index}
+            block={block}
+            onAddBlock={handleAddBlock}
+          />
+        );
       })}
     </>
   );
