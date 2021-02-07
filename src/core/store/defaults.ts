999043fe1a69964a81f069362472ec27f2e8d08f
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-02-07 22:52:23 +0000

    feat(store): set initial store state

---
+++ src/core/store/defaults.ts
@@ -0,0 +1,10 @@
+import { generateId } from '../../utils';
+
+import { Block } from '../../components/editable-block';
+
+export function defaultBlock(): Block {
+  return <Block>{
+    id: generateId(),
+    content: 'Type / for commands',
+  };
+}
--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -1,9 +1,19 @@
 import { BehaviorSubject } from 'rxjs';
 
+import { defaultBlock } from './defaults';
+
 import { Block } from '../../components/editable-block';
 
 class Store {
   state = new BehaviorSubject<Block[]>([]);
+
+  constructor() {
+    this.initStore();
+  }
+
+  private initStore() {
+    this.state.next([defaultBlock()]);
+  }
 
   public getState() {
     return this.state;
