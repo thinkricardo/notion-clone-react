f4a2a86583938517c052642020c21032d5b3f2f4
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-02-08 00:08:52 +0000

    refactor(page): remove action handlers

--- src/components/editable-block/editable-block.tsx
+++ src/components/editable-block/editable-block.tsx
@@ -1,18 +1,14 @@
 import React, { KeyboardEvent, useRef, useState } from 'react';
 
+import { store } from '../../core/store';
+
 import { Block } from './block';
 
 type EditableBlockProps = {
   block: Block;
-  onAddBlock: (afterBlockId: string) => void;
-  onUpdateBlock: (block: Block) => void;
 };
 
-export const EditableBlock: React.FC<EditableBlockProps> = ({
-  block,
-  onAddBlock,
-  onUpdateBlock,
-}) => {
+export const EditableBlock: React.FC<EditableBlockProps> = ({ block }) => {
   const elementRef = useRef<HTMLDivElement>(null);
 
   const [value, setValue] = useState(block);
@@ -26,13 +22,13 @@ export const EditableBlock: React.FC<EditableBlockProps> = ({
     newBlock.content = elementRef.current.innerHTML;
 
     setValue(newBlock);
-    onUpdateBlock(newBlock);
+    store.updateBlock(newBlock);
   };
 
   const handleKeyDown = (evt: KeyboardEvent) => {
     if (evt.key === 'Enter' && !evt.shiftKey) {
       evt.preventDefault();
-      onAddBlock(block.id);
+      store.addBlock(block);
     }
   };
 
--- src/components/editable-page/editable-page.tsx
+++ src/components/editable-page/editable-page.tsx
@@ -12,31 +12,12 @@ export const EditablePage: React.FC = () => {
     return () => subscription.unsubscribe();
   }, []);
 
-  const handleAddBlock = (afterBlockId: string) => {
-    const block = blocks.find((innerBlock) => innerBlock.id === afterBlockId);
-
-    if (block) {
-      store.addBlock(block);
-    }
-  };
-
-  const handleUpdateBlock = (block: Block) => {
-    store.updateBlock(block);
-  };
-
   return (
     <>
       <h2>Page</h2>
 
       {blocks.map((block) => {
-        return (
-          <EditableBlock
-            key={block.id}
-            block={block}
-            onAddBlock={handleAddBlock}
-            onUpdateBlock={handleUpdateBlock}
-          />
-        );
+        return <EditableBlock key={block.id} block={block} />;
       })}
     </>
   );
