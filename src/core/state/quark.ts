f89e32d7531f25601888d969ed100f7a2ce1f31b
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-02-12 00:15:21 +0000

    feat(state): add listeners to handle quark changes

--- src/core/state/quark.ts
+++ src/core/state/quark.ts
@@ -1,8 +1,12 @@
+import { BehaviorSubject, Subscription } from 'rxjs';
+
 export class Quark<T> {
   private value: T;
+  private listeners: BehaviorSubject<T>;
 
   constructor(initialValue: T) {
     this.value = initialValue;
+    this.listeners = new BehaviorSubject(this.value);
   }
 
   get(): T {
@@ -11,5 +15,14 @@ export class Quark<T> {
 
   set(newValue: T): void {
     this.value = newValue;
+    this.listeners.next(this.value);
+  }
+
+  subscribe(listener: (value: T) => void): Subscription {
+    return this.listeners.subscribe(listener);
+  }
+
+  unsubscribe(subscription: Subscription): void {
+    subscription.unsubscribe();
   }
 }
