826a68342574352fabd91f461cec7a672f406de7
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-02-12 00:16:28 +0000

    feat(state): subscribe hook to quark changes

--- src/core/state/quark-hooks.ts
+++ src/core/state/quark-hooks.ts
@@ -1,8 +1,17 @@
+import { useEffect, useState } from 'react';
+
 import { Quark } from './quark';
 import { quarkState } from './quark-state';
 
 export const useQuarkState = <T>(key: string): [T, (newValue: T) => void] => {
   const instance: Quark<T> = quarkState.get<T>(key);
 
-  return [instance.get(), (newValue: T) => instance.set(newValue)];
+  const [innerValue, setInnerValue] = useState<T>(instance.get());
+
+  useEffect(() => {
+    const subscription = instance.subscribe(setInnerValue);
+    return () => instance.unsubscribe(subscription);
+  }, [instance, setInnerValue]);
+
+  return [innerValue, (newValue: T) => instance.set(newValue)];
 };
