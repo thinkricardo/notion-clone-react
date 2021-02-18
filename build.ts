5b08ee0263aa25bcc634bc249afcae8cda3e003b
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-02-18 18:44:13 +0000

    build(app): start file watcher and rebuild sources

--- build.ts
+++ build.ts
@@ -1,19 +1,38 @@
-import { build } from 'esbuild';
+import chokidar from 'chokidar';
+import { build, BuildIncremental } from 'esbuild';
 import liveServer from 'live-server';
 
 const serverPort = 1234;
 const serverRoot = 'build';
 
-const entryPoint = './src/index.tsx';
+const sourceRoot = 'src';
+const entryPoint = `./${sourceRoot}/index.tsx`;
 const outFile = `./${serverRoot}/index.js`;
 
+let builder: BuildIncremental;
+
 const initBuild = async () => {
-  await build({
+  builder = await build({
     bundle: true,
     entryPoints: [entryPoint],
     outfile: outFile,
+    incremental: true,
     define: { 'process.env.NODE_ENV': '"development"' },
   });
+};
+
+const initWatcher = () => {
+  chokidar
+    .watch(`./${sourceRoot}/**/*.{ts,tsx}`, {
+      ignoreInitial: true,
+      interval: 0,
+    })
+    .on('add', () => {
+      builder.rebuild();
+    })
+    .on('change', () => {
+      builder.rebuild();
+    });
 };
 
 const startServer = () => {
@@ -26,6 +45,7 @@ const startServer = () => {
 
 const run = async () => {
   await initBuild();
+  initWatcher();
   startServer();
 };
 
