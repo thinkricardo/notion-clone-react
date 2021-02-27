e0bc25b8bcb1bb063a49288280f986b152a6e229
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-02-27 15:38:57 +0000

    feat(menu): add base menu styling

---
+++ src/components/menu/menu.styles.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const MenuWrapper = styled.div`
+  width: 180px;
+  color: rgb(55, 53, 47);
+  font-size: 0.9rem;
+`;
--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
+import { MenuWrapper } from './menu.styles';
+
 import { MenuItem } from './menu-item';
 import { MenuTitle } from './menu-title';
 
 export const Menu: React.FC = () => {
   return (
-    <div>
+    <MenuWrapper>
       <MenuTitle>Basic blocks</MenuTitle>
 
       <MenuItem>Text</MenuItem>
       <MenuItem>Heading 1</MenuItem>
       <MenuItem>Heading 2</MenuItem>
       <MenuItem>Heading 3</MenuItem>
-    </div>
+    </MenuWrapper>
   );
 };
