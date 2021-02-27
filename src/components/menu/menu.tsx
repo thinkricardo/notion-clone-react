d7d55da1d0e477622afb0273726f6b6c56c76057
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-02-27 00:29:32 +0000

    feat(menu): render sample menu items

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { MenuItem } from './menu-item';
+
 export const Menu: React.FC = () => {
   return (
     <div>
       <h6>Basic blocks</h6>
+
+      <MenuItem>Text</MenuItem>
+      <MenuItem>Heading 1</MenuItem>
+      <MenuItem>Heading 2</MenuItem>
+      <MenuItem>Heading 3</MenuItem>
     </div>
   );
 };
