d8282efac0ce55c9e50eb137416b60abfa3da0f8
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-03-02 22:27:06 +0000

    feat(menu): add padding and reduce font size to menu item

--- src/components/menu/menu-item.styles.tsx
+++ src/components/menu/menu-item.styles.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const MenuItemWrapper = styled.div`
+  padding: 8px 0;
+  font-size: 0.8rem;
+
   &:hover {
     background: #efefef;
     cursor: pointer;
