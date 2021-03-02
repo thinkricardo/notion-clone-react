e17be51e8a62128e462b91b64a560d70a6f47883
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-03-02 22:07:06 +0000

    feat(app): center app in screen

---
+++ src/containers/app/app.styles.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Helvetica, Verdana, Arial, sans-serif;
+`;
