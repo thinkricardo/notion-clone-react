e3da3f80312bf988b233b6b20dcb7e6aabbd6d20
Ricardo Pereira <ricardo.mlpp@gmail.com>
2021-03-02 22:26:38 +0000

    feat(menu): add border and padding to menu

--- src/components/menu/menu.styles.tsx
+++ src/components/menu/menu.styles.tsx
@@ -7,6 +7,9 @@ type MenuWrapperProps = {
 export const MenuWrapper = styled.div<MenuWrapperProps>`
   display: ${(props) => (props.isVisible ? 'block' : 'none')};
   width: 180px;
+  padding: 8px 12px;
   color: rgb(55, 53, 47);
   font-size: 0.9rem;
+  border: solid 1px rgb(220, 220, 220);
+  border-radius: 3px;
 `;
